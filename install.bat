55c43398f1977583972a1e7b72d246953d94b703
khaosnmt <khaosnmt@users.noreply.github.com>
2018-10-07 19:28:16 -0600

    v1.1a
    Added config files which allows for removing hardcoded creds and IP from the batch file

---
+++ install.bat
@@ -0,0 +1,79 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 4
+:: Automatically check & get admin rights
+::::::::::::::::::::::::::::::::::::::::::::
+ @echo off
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
+ REM Run shell as admin (example) - put here code as you like
+ ECHO %batchName% Arguments: P1=%1 P2=%2 P3=%3 P4=%4 P5=%5 P6=%6 P7=%7 P8=%8 P9=%9
+ reg import "%ProgramFiles%\Click2Call\registry\tel_handler.reg"
+ move "%ProgramFiles%\Click2Call\curl.exe" "C:\Windows\System32\curl.exe"
+
+
+    call :MsgBox "We need to reboot to finish the installation."  "VBYesNo+VBQuestion" "Click yes to reboot now"
+    if errorlevel 7 (
+        echo NO - don't reboot now
+    ) else if errorlevel 6 (
+        echo YES - reboot
+        start "" "shutdown /r /t 0"
+    )
+
+    exit /b
+
+:MsgBox prompt type title
+    setlocal enableextensions
+    set "tempFile=%temp%\%~nx0.%random%%random%%random%vbs.tmp"
+    >"%tempFile%" echo(WScript.Quit msgBox("%~1",%~2,"%~3") & cscript //nologo //e:vbscript "%tempFile%"
+    set "exitCode=%errorlevel%" & del "%tempFile%" >nul 2>nul
+    endlocal & exit /b %exitCode%
--- polycom_click2call.bat
+++ polycom_click2call.bat
@@ -1,6 +1,16 @@
 @echo on
 
-REM Make a temporary variable to store the number being fed by the commandline
+:: IP address variable
+:: Edit this to match the IP of the phone you're controlling
+set /p ip=<settings\ip.conf
+
+:: Push message credentials
+set /p credentials=<settings\credentials.conf
+
+
+:: DO NOT EDIT BELOW THIS LINE!!!
+
+:: Make a temporary variable to store the number being fed by the commandline
 set phone=%1
 
-curl -k --digest -u polycom:polycom -d "<PolycomIPPhone><Data priority=\"Critical\">%phone%</Data></PolycomIPPhone>" --header "Content-Type: application/x-com-polycom-spipx" https://192.168.2.67/push
+curl -k --digest -u %credentials% -d "<PolycomIPPhone><Data priority=\"Critical\">%phone%</Data></PolycomIPPhone>" --header "Content-Type: application/x-com-polycom-spipx" https://%ip%/push
